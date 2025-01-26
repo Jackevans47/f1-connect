e38e235dd33eb80b8236291cb9ad8a816936f34e
jackevans47 <jack_evans47@outlook.com>
2025-01-26 16:28:47 +0000

    add log in and out features

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = "https://f1-social-api-910f5cff82da.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
